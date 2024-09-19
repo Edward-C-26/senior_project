679b6adc9ee28b0752e7dd5b7960df22801672c0
David Lacayo <davidlacayo2@gmail.com>
2024-09-19 11:27:32 -0500

    Hopefully this fixes stuff

---
+++ BMS_2024/Core/Inc/BMSconfig.h
@@ -0,0 +1,61 @@
+#ifndef BMS_CONFIG_H
+#define BMS_CONFIG_H
+
+#include "main.h"
+#include "stdbool.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define NUM_CELLS	144
+#define NUM_BOARDS	12
+#define NUM_TEMPS	4
+
+typedef struct {
+	// General BMS configuration
+	uint8_t numOfICs;
+	uint8_t address[16];
+	uint8_t addressesofICs[16];
+	uint8_t numOfCellInputs;
+	uint8_t numOfCellsPerIC;
+	uint8_t numOfTempPerIC;
+	uint8_t ADCConversionRate;
+	uint16_t OV_threshold;
+	uint16_t UV_threshold;
+	uint16_t LUV_threshold;
+	uint16_t OT_threshold;
+	uint16_t UT_threshold;
+	uint16_t HUV_threshold;
+	uint16_t slowCharge_threshold;
+	uint16_t stopCharge_threshold;
+	uint16_t max_difference;
+	uint16_t balancing_difference;
+	uint16_t balancing_start_threshold;
+	uint8_t invalidPECcount;
+	uint16_t dischargeTime;
+	uint16_t start_scaling;
+	uint16_t stop_scaling;
+	uint16_t scale_to;
+	uint16_t normalCurrent;
+	uint16_t lowerCurrent;
+	uint16_t chargerVoltage;
+
+	// LTC configuration
+	bool GPIO5PulldownOff;
+	bool GPIO4PulldownOff;
+	bool GPIO3PulldownOff;
+	bool GPIO2PulldownOff;
+	bool GPIO1PulldownOff;
+	bool ReferenceOn;
+	bool ADCModeOption;
+	uint16_t UndervoltageComparisonVoltage;
+	uint16_t OvervoltageComparisonVoltage;
+	bool DischargeCell[12];
+	uint8_t DischargeTimeoutValue;
+	uint8_t ADCMode;
+	bool DischargePermitted;
+
+} BMSConfigStructTypedef;
+
+void loadConfig(BMSConfigStructTypedef* config);
+
+#endif	// BMS_CONFIG_H
---
+++ BMS_2024/Core/Inc/PackCalculations.h
@@ -0,0 +1,15 @@
+#ifndef PACK_CALCULATIONS
+#define PACK_CALCULATIONS
+
+#include "BMSconfig.h"
+#include "LTC6811.h"
+// #include "LTC2949.h"
+#include "Fault.h"
+
+#define MAXINT16 65535
+
+void setCriticalVoltages(BMSConfigStructTypedef *cfg, BMS_critical_info_t *bms, uint8_t bmsData[144][6]);
+void setCriticalTemps(BMSConfigStructTypedef *cfg, BMS_critical_info_t *bms, uint8_t bmsData[144][6]);
+void balance(BMSConfigStructTypedef *cfg, BMS_critical_info_t *bms, uint8_t bmsData[144][6], bool cellDischarge[12][12], bool fullDischarge[12][12], uint8_t balanceCounter, uint8_t chargeRate);
+
+#endif  // PACK_CALCULATIONS
