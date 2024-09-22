bc9b5139e43fd4dceca88690dd5c96b2e9f899d9
Mira Panigrahy <mirapanigrahy@Miras-Laptop.local>
2024-09-22 11:06:32 -0500

    added netid to packcalculations

--- BMS_2024/Core/Src/PackCalculations.c
+++ BMS_2024/Core/Src/PackCalculations.c
@@ -2,6 +2,7 @@
  * Last Edited Spring 2024 : David Lacayo
 */
 #include "PackCalculations.h"
+uint8_t mira4 = 1;
 
 
 //! \brief This method calculates the maximum and minimum cell voltages in the pack, and sets those values w/ the associated
