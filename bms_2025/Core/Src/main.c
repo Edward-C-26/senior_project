908477e36f1a2b6b44905c57634f470ba4e79c03
akashc3 <akashc3@illinois.edu>
2025-05-06 22:29:18 -0500

    128 Hz for real

--- bms_2025/Core/Src/main.c
+++ bms_2025/Core/Src/main.c
@@ -210,11 +210,12 @@ int main(void)
     loadConfig(&BMSConfig);
     init_BMS_info(&BMSCriticalInfo);
     resetChargerVariables();
-//    isoADC_rdy_status = wakeup_isoADC(&gIsoADCConfig, &gIsoADCData);
+
+    // wake up isoADC
     HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
     isoADC_rdy_status = wakeup_isoADC(&gIsoADCConfig, &gIsoADCData);
 
-
+    // turn on timer interrupt for isoADC 128Hz
     HAL_TIM_Base_Start_IT(&htim1);
 
   /* USER CODE END 2 */
@@ -640,9 +641,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 3;
+  htim1.Init.Prescaler = 1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 39999;
+  htim1.Init.Period = 62499;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
@@ -874,7 +875,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     convert_raw_to_actual(&gIsoADCConfig, &gIsoADCData);
     BMSCriticalInfo.packVoltage = (uint16_t)gIsoADCData.bus_voltage;
     HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
-    __HAL_TIM_SET_COUNTER(&htim1, 0);
+//    __HAL_TIM_SET_COUNTER(&htim1, 0);/
+    // send packstat message
 //    PACKSTAT_message(&BMSCriticalInfo);
 }
 
